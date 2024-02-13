54922883b852a77439c4d15e2184ea2cd378d2ce
kavishnx2 <kavish.mojhoa@nx2square.com>
2024-02-13 14:14:01 +0400

    added systemn log in local storage

--- assets/js/logger.js
+++ assets/js/logger.js
@@ -1,31 +1,30 @@
 function getCurrentTimestamp() {
     return new Date().toISOString();
-  }
-  
-  function logPageVisit(page) {
+}
+
+function logPageVisit(page) {
     try {
-      let spanElement = document.querySelector('.d-none.d-md-block.dropdown-toggle.ps-2');
-      let username = spanElement ? spanElement.textContent : "Anonymous";
-  
-      // Retrieve existing log data from localStorage
-      var logData = JSON.parse(localStorage.getItem("pageVisitLog")) || [];
-  
-      // Create log entry
-      var logEntry = {
-        "user": username,
-        "timestamp": getCurrentTimestamp(),
-        "page": page
-      };
-  
-      logData.push(logEntry);
-  
-      localStorage.setItem("pageVisitLog", JSON.stringify(logData));
-      console.log("Page visit logged:", logEntry);
+        let spanElement = document.querySelector('.d-none.d-md-block.dropdown-toggle.ps-2');
+        let username = spanElement ? spanElement.textContent : "Anonymous";
+
+        // Retrieve existing log data from localStorage
+        var logData = JSON.parse(localStorage.getItem("pageVisitLog"));
+
+        // Create log entry
+        var logEntry = {
+            "user": username,
+            "timestamp": getCurrentTimestamp(),
+            "page": page
+        };
+
+        logData.push(logEntry);
+
+        localStorage.setItem("pageVisitLog", JSON.stringify(logData));
     } catch (error) {
-      console.error("Error logging page visit:", error);
+        console.error("Error logging page visit:", error);
     }
-  }
-  
-  window.addEventListener("load", function () {
+}
+
+window.addEventListener("load", function () {
     logPageVisit(window.location.href);
-  });
+});
