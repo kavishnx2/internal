7dd7b24333bc49cebed95cb058378df757136960
kavishnx2 <kavish.mojhoa@nx2square.com>
2024-02-13 11:17:46 +0400

    ,

--- assets/js/logger.js
+++ assets/js/logger.js
@@ -1,21 +1,31 @@
 function getCurrentTimestamp() {
     return new Date().toISOString();
-}
-
-function logPageVisit(page) {
-
-    var username = document.getElementById("userProfileName").value;
-    
-    // Create log object
-    var logEntry = {
+  }
+  
+  function logPageVisit(page) {
+    try {
+      let spanElement = document.querySelector('.d-none.d-md-block.dropdown-toggle.ps-2');
+      let username = spanElement ? spanElement.textContent : "Anonymous";
+  
+      // Retrieve existing log data from localStorage
+      var logData = JSON.parse(localStorage.getItem("pageVisitLog")) || [];
+  
+      // Create log entry
+      var logEntry = {
         "user": username,
         "timestamp": getCurrentTimestamp(),
         "page": page
-    };
-
-    console.log(logEntry);
-}
-
-window.addEventListener("load", function() {
+      };
+  
+      logData.push(logEntry);
+  
+      localStorage.setItem("pageVisitLog", JSON.stringify(logData));
+      console.log("Page visit logged:", logEntry);
+    } catch (error) {
+      console.error("Error logging page visit:", error);
+    }
+  }
+  
+  window.addEventListener("load", function () {
     logPageVisit(window.location.href);
-});
+  });
